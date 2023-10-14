3ed264fdb080c6bd13310ce7661d26aa26b82d60
지상은 <87356953+topsilverr@users.noreply.github.com>
2023-10-14 23:36:08 +0900

    [Gold V] Title: 강의실 배정, Time: 448 ms, Memory: 73448 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,27 +1,28 @@
-# heapq 사용
 import sys,heapq
 
 input = sys.stdin.readline
 
 n = int(input())
-
 cls = []
-endTime = []
-
 for _ in range(n):
-    cls.append(list(map(int, input().split())))
+    cls.append(list(map(int,input().split())))
+
+res = 1
 
 cls.sort()
 
-heapq.heappush(endTime,cls[0][1])
-res = 1 # 전체 강의실 개수
+# end = [0] * n
+# end[1] = cls[0][1]
+# 이거를 heapq 로?
+end = []
+heapq.heappush(end,cls[0][1])
 
 for i in range(1,n):
-    if cls[i][0] < endTime[0]:
+    if end[0] > cls[i][0]:
         res+=1
-        heapq.heappush(endTime,cls[i][1])
+        heapq.heappush(end,cls[i][1])
     else:
-        heapq.heappop(endTime)
-        heapq.heappush(endTime,cls[i][1])
+        heapq.heappop(end)
+        heapq.heappush(end,cls[i][1])
 
 print(res)

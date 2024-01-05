1e05f7a4f84eed41f3a274e192e55b8747416891
지상은 <87356953+topsilverr@users.noreply.github.com>
2024-01-05 20:52:49 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,14 @@
+SELECT
+    BOARD_ID
+    , WRITER_ID
+    , TITLE
+    , PRICE
+    , 
+    case when STATUS = "SALE" then "판매중"
+    when STATUS = "RESERVED" then "예약중"
+    when STATUS = "DONE" then "거래완료"
+    end STATUS
+
+from USED_GOODS_BOARD
+where CREATED_DATE = "2022-10-05%"
+order by 1 desc

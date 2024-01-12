a0ce09bf25e87e98d4f13b165e06a6ceb0c9c4c2
지상은 <87356953+topsilverr@users.noreply.github.com>
2024-01-12 18:52:38 +0900

    [level Lv.3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT o.animal_id, o.name
+from animal_outs o
+left join animal_ins i on o.animal_id = i.animal_id
+order by (DATEDIFF(o.datetime,i.datetime)) desc
+limit 2
+
+
+# SELECT O.ANIMAL_ID, O.NAME
+# FROM ANIMAL_OUTS O
+# LEFT JOIN ANIMAL_INS I
+# ON O.ANIMAL_ID = I.ANIMAL_ID
+# ORDER BY (DATEDIFF(O.DATETIME,I.DATETIME)) DESC
+# LIMIT 2
